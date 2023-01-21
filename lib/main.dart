de08407156de24899988a57d77d9a38498ed2c45
Jb_jaydeep <jaydeepborda846@gmail.com>
2023-01-21 14:12:47 +0530

    LEcture-3.2

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Center(
+          child: Text("Hello\n\n\n\nDart\n\n\n\nFlutter"),
+        ),
+      ),
+    ),
+  );
+}
