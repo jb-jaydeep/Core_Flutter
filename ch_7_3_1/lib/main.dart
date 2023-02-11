3c3a6274c0c60a0d403ea563af40547e46b113d9
jb_jaydeep <jaydeepborda846@gmail.com>
2023-02-12 01:48:23 +0530

    ch-7-3

---
+++ ch_7_3_1/lib/main.dart
@@ -0,0 +1,23 @@
+
+import 'package:ch_7_3_1/views/homepage.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(
+    const Myapp(),
+  );
+}
+
+class  Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: homepage(),
+
+    );
+  }
+}
