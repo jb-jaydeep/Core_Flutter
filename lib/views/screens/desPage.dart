52be731c7d466d0809ae0d83d4de33ad97cfee10
jb_jaydeep <jaydeepborda846@gmail.com>
2023-03-04 14:33:39 +0530

    ch-10-2

--- lib/views/screens/desPage.dart
+++ lib/views/screens/desPage.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class desPage extends StatefulWidget {
-
-  const desPage( {Key? key, }) : super(key: key);
+  const desPage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<desPage> createState() => _desPageState();
@@ -11,8 +12,7 @@ class desPage extends StatefulWidget {
 class _desPageState extends State<desPage> {
   @override
   Widget build(BuildContext context) {
-
-     Map data = ModalRoute.of(context)!.settings.arguments as Map;
+    Map data = ModalRoute.of(context)!.settings.arguments as Map;
     Size s = MediaQuery.of(context).size;
     double h = s.height;
     double w = s.width;
@@ -20,8 +20,8 @@ class _desPageState extends State<desPage> {
       appBar: AppBar(
         centerTitle: true,
         title: Text(
-         data['title'],
-          style: TextStyle(
+          data['title'],
+          style: const TextStyle(
             color: Colors.white,
             fontSize: 25,
             fontWeight: FontWeight.bold,
@@ -29,101 +29,104 @@ class _desPageState extends State<desPage> {
         ),
         backgroundColor: Colors.black,
       ),
-          body: Container(
-            margin: const EdgeInsets.all(10),
-            child: Column(
-
-        children: [
+      body: Container(
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
             Expanded(
               flex: 4,
               child: Container(
                 width: w,
                 margin: const EdgeInsets.all(5),
-                child: Image.asset(data['logo'] ),
                 decoration: const BoxDecoration(
                   color: Colors.white,
                   border: Border(
-                    bottom: BorderSide(
-                      color: Colors.red,
-                      width: 2,
-                    )
-                  ),
+                      bottom: BorderSide(
+                    color: Colors.red,
+                    width: 2,
+                  )),
                   boxShadow: [
                     BoxShadow(
                       blurRadius: 3,
                     ),
                   ],
                 ),
+                child: Image.asset(data['logo']),
               ),
             ),
             Expanded(
               flex: 2,
               child: Container(
                 alignment: Alignment.center,
-                margin: EdgeInsets.all(5),
-               // padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
+                margin: const EdgeInsets.all(5),
+                // padding: EdgeInsets.all(10),
+                decoration: const BoxDecoration(
                   color: Colors.white,
                   border: Border(
                       bottom: BorderSide(
-                        color: Colors.red,
-                        width: 2,
-                      )
-                  ),
+                    color: Colors.red,
+                    width: 2,
+                  )),
                   boxShadow: [
                     BoxShadow(
                       blurRadius: 3,
                     ),
                   ],
                 ),
-                child:ListTile(
-                 contentPadding: EdgeInsets.only(left: 80),
-                  title: Text(data['title'] ?? 'No title',style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 35,
-                  ),),
-                  subtitle: Text(data['subtitle'] ?? 'No title',style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25,
-                  ),),
-
+                child: ListTile(
+                  contentPadding: const EdgeInsets.only(left: 80),
+                  title: Text(
+                    data['title'],
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35,
+                    ),
+                  ),
+                  subtitle: Text(
+                    data['subtitle'],
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                    ),
+                  ),
                 ),
-
               ),
             ),
             Expanded(
               flex: 4,
-              child: Container(
-                alignment: Alignment.center,
-
-                margin: EdgeInsets.all(5),
-                decoration: BoxDecoration(
-                  
-                  color: Colors.white,
-                  border: Border(
-                      bottom: BorderSide(
-                        color: Colors.red,
-                        width: 2,
-                      )
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(20),
+                  margin: const EdgeInsets.all(5),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    border: Border(
+                        bottom: BorderSide(
+                      color: Colors.red,
+                      width: 2,
+                    )),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 3,
+                      ),
+                    ],
                   ),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 3,
+                  child: Text(
+                    data['des'],
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      color: Colors.black,
                     ),
-                  ],
+                  ),
                 ),
-              child: Text(data['des'],style: TextStyle(
-
-                color: Colors.black,
-              ),),
-
               ),
             ),
-        ],
-    ),
-          ),
-
-
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/views/screens/homepage.dart
+++ lib/views/screens/homepage.dart
@@ -34,50 +34,57 @@ class _homePageState extends State<homePage> {
       'title': "Deeksha Joshi",
       'subtitle': "Actor",
       'subtitlea': "",
-      'des': "Deeksha debuted with the movie Shubh Aarambh, directed by Amit Barot in 2017. She then starred in other two movies Karsandas Pay & Use and Colorbaaj in the same year.[2] Karsandas Pay & Use was her first Commercially successful filmHer next project was Sharato Lagu alongside Malhar Thakkar. This was also a commercial success. She won the GIFA best actress of the year in 2018 for her role in Sharato Lagu. The film and her performance was both critically and commercially acclaimed. Her next venture was Dhunki starring with Pratik Gandhi which was acclaimed critically.[3] In 2020, Deeksha received the Critics' Choice Film Awards Best Actress (Gujarati) for Dhunki. Deeksha did the titular role in the romantic comedy film Luv Ni Love Storys directed by Durgesh Tanna which was also released in 2020.[4][5]",
+      'des':
+          "Deeksha debuted with the movie Shubh Aarambh, directed by Amit Barot in 2017. She then starred in other two movies Karsandas Pay & Use and Colorbaaj in the same year.[2] Karsandas Pay & Use was her first Commercially successful filmHer next project was Sharato Lagu alongside Malhar Thakkar. This was also a commercial success. She won the GIFA best actress of the year in 2018 for her role in Sharato Lagu. The film and her performance was both critically and commercially acclaimed. Her next venture was Dhunki starring with Pratik Gandhi which was acclaimed critically.[3] In 2020, Deeksha received the Critics' Choice Film Awards Best Actress (Gujarati) for Dhunki. Deeksha did the titular role in the romantic comedy film Luv Ni Love Storys directed by Durgesh Tanna which was also released in 2020.[4][5]",
     },
     {
       'logo': (imagePath + i3),
       'title': "Dilip Joshi",
       'subtitle': "Actor",
       'subtitlea': "",
-      'des': "",
+      'des':
+          "Dilip Joshi (born 26 May 1968)[3] is an Indian film, television actor. He has appeared in several Indian movies and television series. Joshi has performed mostly in comedy roles, he initially worked in side roles in Bollywood films and then subsequently garnered national fame for his portrayal of Jethalal Champaklal Gada in the Indian sitcom Taarak Mehta Ka Ooltah Chashmah.",
     },
     {
       'logo': (imagePath + i4),
-      'title': "Janki",
+      'title': "Janki Bodiwala",
       'subtitle': "Actor",
-      'subtitlea': "",
+      'subtitlea': "Janki was born in Ahmedabad, Gujarat to Bharat Bodiwala and Kashmira Bodiwala. She has a brother Dhrupad Bodiwala.She has done her schooling from M K Secondary & Higher Secondary School, Ahmedabad. She did not complete her graduation in Bachelor of Dental Science (BDS) from Goenka Research Institute of Dental Science, Gandhinagar.[3] She also participated in Miss India 2019 where she was in the top 3 finalist of Miss India Gujarat.",
       'des': "",
     },
     {
       'logo': (imagePath + i5),
       'title': "Malhar Thakar",
       'subtitle': "Actor",
-      'subtitlea': "",
-      'des': "",
+      'subtitlea':"",
+      'des':"",
     },
     {
       'logo': (imagePath + i6),
       'title': "Naresh Kanodiya",
       'subtitle': "Actor",
       'subtitlea': "",
-      'des': "",
+      'des': "He studied at Shree Ambe Vidyalaya and Sheth C. N."
+    " Vidyalaya, AhmedabadAfter nine years in theatre, "
+    "he eventually broke into film roles. His debut film as "
+    "the lead role was Chhello Divas (2015) which was"
+    " commercially successful."
+    "His Love Ni Bhavai (2017) achieved critical"
+    " and commercial success and more than 100 days into theatres."
+    " Golkeri (2020) was also declared hit.",
     },
     {
       'logo': (imagePath + i7),
       'title': "Snehlata",
       'subtitle': "Actor",
-      'subtitlea': "",
+      'subtitlea': "Snehlata is Indian Actress, who works in Gujarati Cinemas. She is very popular Actress of Gujarati Movies.&nbsp;She has acted in Hindi cinema as well as Snehlata has appeared in many Hindi Movies and Gujarati Movies like Bhagwan Shri Krishna, Dhola Maru, Sampoorna Mahabharat, Garvo Garasiyo, Jogi, Natak, Hothal Padmini, Harishchandra Taramati, Ram Rajya, Mere Hamdam Mere Dost, Be-Imaan and More.",
       'des': "",
     },
   ];
 
   @override
   Widget build(BuildContext context) {
-    Size s = MediaQuery
-        .of(context)
-        .size;
+    Size s = MediaQuery.of(context).size;
     double h = s.height;
     double w = s.width;
     return Scaffold(
@@ -93,135 +100,135 @@ class _homePageState extends State<homePage> {
       body: Center(
         child: Column(
           children: [
-
             Container(
               alignment: Alignment(-0.7, 0),
               color: Color(0xff060047),
               height: h * 0.1,
-              child: Text("Actors", style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),),),
-
+              child: Text(
+                "Actors",
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
             SingleChildScrollView(
               scrollDirection: Axis.horizontal,
               child: Row(
-                children: Data
-                    .map((e) =>
-                    Stack(
-                      children: [
-                        Container(
-                          height: h * 0.7,
-                          width: w * 0.7,
-                          color: Color(0xff060047),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Container(
-                                margin: EdgeInsets.all(10),
-                                height: 350,
-                                width: 250,
-                                child: Positioned(
-                                  //top: 10,
-                                  left: 10,
-                                  bottom: 100,
+                children: Data.map(
+                  (e) => Stack(
+                    children: [
+                      Container(
+                        height: h * 0.7,
+                        width: w * 0.7,
+                        color: Color(0xff060047),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              margin: EdgeInsets.all(10),
+                              height: 350,
+                              width: 250,
+                              child: Positioned(
+                                //top: 10,
+                                left: 10,
+                                bottom: 100,
 
-                                  child: Container(
-                                    alignment: const Alignment(0, -4),
-                                    margin: EdgeInsets.all(5),
-                                    width: 250,
-                                    height: 280,
-                                    padding: const EdgeInsets.all(20),
-                                    decoration: BoxDecoration(
-                                      color: Color(0xff301E67),
-                                      border: Border.all(
-                                        width: 3,
-                                      ),
-                                      boxShadow: const [
-                                        BoxShadow(
-                                          color: Colors.black,
-                                          spreadRadius: 8,
-                                          blurRadius: 20,
-                                        )
-                                      ],
+                                child: Container(
+                                  alignment: const Alignment(0, -4),
+                                  margin: EdgeInsets.all(5),
+                                  width: 250,
+                                  height: 280,
+                                  padding: const EdgeInsets.all(20),
+                                  decoration: BoxDecoration(
+                                    color: Color(0xff301E67),
+                                    border: Border.all(
+                                      width: 3,
                                     ),
-                                    child: SizedBox(
-                                      height: 200,
-                                      width: 200,
-                                      child: Image.asset(e['logo']),
-                                    ),
+                                    boxShadow: const [
+                                      BoxShadow(
+                                        color: Colors.black,
+                                        spreadRadius: 8,
+                                        blurRadius: 20,
+                                      )
+                                    ],
+                                  ),
+                                  child: SizedBox(
+                                    height: 200,
+                                    width: 200,
+                                    child: Image.asset(e['logo']),
                                   ),
                                 ),
                               ),
-                            ],
-                          ),
-                        ),
-
-                        Positioned(
-                          left: 30,
-                          top: 300,
-                          child: Text(
-                            e['title'],
-                            style: TextStyle(
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
                             ),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        left: 30,
+                        top: 300,
+                        child: Text(
+                          e['title'],
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
                           ),
                         ),
-                        Positioned(
-                          left: 30,
-                          top: 340,
-                          child: Text(
-                            e['subtitle'],
-                            style: TextStyle(
-                              fontSize: 21,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey,
-                            ),
+                      ),
+                      Positioned(
+                        left: 30,
+                        top: 340,
+                        child: Text(
+                          e['subtitle'],
+                          style: TextStyle(
+                            fontSize: 21,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey,
                           ),
                         ),
-                        Positioned(
-                          left: 30,
-                          top: 390,
-                          child: GestureDetector(
-                            onTap: () {
-                               Navigator.of(context).pushNamed('page1',arguments: e);
-                             // Navigator.of(context).push(MaterialPageRoute(
-                               //   builder: (context) => desPage(data: e,)),);
-                            },
-                            child: Container(
-                              height: 50,
-                              width: 130,
-                              alignment: Alignment.center,
-                              child: const Text(
-                                "Know More",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 21,
-                                ),
-                              ),
-                              decoration: BoxDecoration(
-                                color: Colors.black,
-                                border: Border.all(color: Colors.grey),
-                                borderRadius: BorderRadius.circular(20),
-                                boxShadow: const [
-                                  BoxShadow(
-                                    color: Colors.black,
-                                    spreadRadius: 2,
-                                    blurRadius: 20,
-                                  )
-                                ],
+                      ),
+                      Positioned(
+                        left: 30,
+                        top: 390,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.of(context)
+                                .pushNamed('page1', arguments: e);
+                            // Navigator.of(context).push(MaterialPageRoute(
+                            //   builder: (context) => desPage(data: e,)),);
+                          },
+                          child: Container(
+                            height: 50,
+                            width: 130,
+                            alignment: Alignment.center,
+                            child: const Text(
+                              "Know More",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 21,
                               ),
                             ),
+                            decoration: BoxDecoration(
+                              color: Colors.black,
+                              border: Border.all(color: Colors.grey),
+                              borderRadius: BorderRadius.circular(20),
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Colors.black,
+                                  spreadRadius: 2,
+                                  blurRadius: 20,
+                                )
+                              ],
+                            ),
                           ),
                         ),
-
-                      ],
-                    ),)
-                    .toList(),
+                      ),
+                    ],
+                  ),
+                ).toList(),
               ),
             ),
           ],
