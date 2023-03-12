36dc714f3c558b45cf663dbd553f0ab804d807aa
jb-jaydeep <jaydeepborda846@gmail.com>
2023-03-12 15:34:22 +0530

    story_setter

---
+++ lib/views/screebs/image_utils.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+String imagePath = "assets/images/";
+String i = "1.jpg";
+
+
+String i1 = "2.jpg";
+String i2 = "3.jpg";
+String i3 = "4.jpg";
+String i4 = "5.jpg";
+String i5 = "6.jpg";
+String i6 = "7.jpg";
+String i7 = "8.jpg";
+String i8 = "9.jpg";
+String i9 = "10.jpg";
+String i10 = "11.jpg";
